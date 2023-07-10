4b284e9914e136f14e668a9e978da834ba601187
Eros Cedeño <16-10216@usb.ve>
2023-07-09 22:00:17 -0400

    Constrains 1 2 5 6 7 8 added

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ using json = nlohmann::json;
 
 void welcome();
 void print_help(char* pwd);
+void create_cnf_file( std::vector<variable> variables, std::vector<time_t> times, std::string filename );
 
 int main(int argc, char *argv[])
 {
@@ -87,9 +88,10 @@ int main(int argc, char *argv[])
     // std::cout << "Number of variables: " << variables.size() << '\n';
 
     // // Testing accessing to variables from vector
-    test_variables_accessing(variables);
+    // test_variables_accessing(variables);
 
     // Creating .cnf file and filling with restrictions
+    create_cnf_file( variables, times, outfile );
     
 
    return 0;
@@ -103,4 +105,69 @@ void welcome() {
 
 void print_help(char* pwd) {
     printf("\nUSAGE:\n\t%s <JSON-input-file> <output-file>\n", pwd);
+}
+
+void create_cnf_file( std::vector<variable> variables, std::vector<time_t> times, std::string filename ) {
+    std::cout << "Creating CNF file: " << filename << ".cnf " <<  '\n';
+    std::ofstream f(filename + ".cnf");
+    if( !f.is_open() ){
+        std::cout << "Error creating outpufile\n";
+        exit(EXIT_FAILURE);
+    }
+    f << "c\n";
+    f << "c\tGenerated CNF file for " << filename <<  ".ics " <<  " specification\n";
+    f << "c\n";
+    f << "p cnf " << variables.size() << " TODO\n\n";
+    
+    // Basic Constraints
+    // At least 2 games per team
+    f << "c\tAt least 2 games per team\n\n";
+    int i = 0;
+    int m = times.size();
+    while ( i < variables.size())
+    {
+        f << i+1 << " ";
+        // std::cout << (i + 1) % m << '\n';
+
+        if ((i+1) % m == 0)
+        {
+            f << "0\n";
+        }
+        i++;        
+    }
+
+    // Exactly two match per pair
+    f << "\nc\tExactly two match per pair\n\n";
+    int num = variables.size() / times.size();
+    int num_times = times.size();
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num_times; j++)
+        {
+            for (int k = j+1; k < num_times; k++)
+            {
+                f << - (j+1 + i*num_times) << ' ' << -(k+1 + i*num_times) << " 0\n";
+            }
+        }
+    }
+
+    // No simultaneus games
+    f << "\nc\tNo simultaneus games\n\n";
+    for (int i = 0; i < num_times; i++)
+    {
+        for (int j = 0; j < num; j++)
+        {
+            f << -(i+1 + j*num_times) << ' ';
+        }
+        f << "0\n";         
+    }
+    
+
+
+    
+    
+
+
+
+    f.close();
 }
